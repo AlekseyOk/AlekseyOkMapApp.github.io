26c273921bda30e28a1a3742053d6f1183617700
Aleksey <cs-zatopija@mail.ru>
2019-10-20 16:29:05 +0300

    changes

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import { withAuth } from "./auth";
 
-//...rest - передаем атрибуты которые относились к роуту
 export const PrivateRoute = withAuth(
   ({ component: RouteComponent, isAuthorized, loginPath, ...rest }) => (
     <Route
